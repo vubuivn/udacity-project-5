b8780daabd7aa38f69929af25b320c74d422ec7d
Vu QuangBui <Vu.QuangBui@coxautoinc.com>
2022-12-12 11:33:27 +0700

    add first file for project 5

--- client/src/config.ts
+++ client/src/config.ts
@@ -1,5 +1,5 @@
 // TODO: Once your application is deployed, copy an API id here so that the frontend could interact with it
-const apiId = 'mipku2m8h7'
+const apiId = 'tdhwyrken8'
 export const apiEndpoint = `https://${apiId}.execute-api.us-east-1.amazonaws.com/dev`
 
 export const authConfig = {
